02e3f25632b092e9cfeceba159823ee422e2628a
Vitor Vilela <vitorvilella123@gmail.com>
2023-09-29 20:36:01 -0300

    First class: Position

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,9 +1,13 @@
 package app;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Criado!");
+		
+		Position pos = new Position (3, 5);
+		System.out.println(pos);
 
 	}
 
