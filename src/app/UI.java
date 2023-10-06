1f150b950283b1f83504188635364b5aec715dd4
Vitor Vilela <vitorvilella123@gmail.com>
2023-10-06 13:53:15 -0300

    Troca de turno

--- src/app/UI.java
+++ src/app/UI.java
@@ -3,6 +3,7 @@ package app;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -58,6 +59,14 @@ public class UI {
 		}
 		System.out.println("  a b c d e f g h");
 
+	}
+	
+	public static void printMatch (ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces()); // imprimimos o tabuleiro com suas peças
+		System.out.println();
+		System.out.println("Turn: " + chessMatch.getTurn()); // declaramos o turno 
+		System.out.println("Aguardando jogador: " + chessMatch.getCurrentPlayer()); // declaramos qual jogador deve jogar agora
+		
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,11 +8,23 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 
+	private int turn;
+	private Color currentPlayer;
 	private Board board;
 
 	public ChessMatch() {
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
+	}
+
+	public int getTurn() {
+		return turn;
+	}
+
+	public Color getCurrentPlayer() {
+		return currentPlayer;
 	}
 
 	public ChessPiece[][] getPieces() {
@@ -32,55 +44,73 @@ public class ChessMatch {
 		}
 		return mat;
 	}
-	
+
 	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
 		Position position = sourcePosition.toPosition();
 		validateSourcePosition(position);
 		return board.piece(position).possibleMoves();
 	}
-	
-	public ChessPiece performChessMove (ChessPosition sourcePosition, ChessPosition targetPosition) {
-		Position source = sourcePosition.toPosition(); // como iremos inserir a posição no formato de xadrez, temos que converter pra matriz, realizando o método toPosition() sobre o sourcePosition inserido 
-		Position target = targetPosition.toPosition(); // como iremos inserir a posição no formato de xadrez, temos que converter pra matriz, realizando o método toPosition() sobre o targetPosition inserido
-		validateSourcePosition(source); // nesse método validamos a posição de origem. Se n existir, lançamos uma exceção
+
+	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+		Position source = sourcePosition.toPosition(); // como iremos inserir a posição no formato de xadrez, temos que
+														// converter pra matriz, realizando o método toPosition() sobre
+														// o sourcePosition inserido
+		Position target = targetPosition.toPosition(); // como iremos inserir a posição no formato de xadrez, temos que
+														// converter pra matriz, realizando o método toPosition() sobre
+														// o targetPosition inserido
+		validateSourcePosition(source); // nesse método validamos a posição de origem. Se n existir, lançamos uma
+										// exceção
 		validateTargetPosition(source, target);
-		Piece capturedPiece = makeMove(source,target); 
-		return (ChessPiece) capturedPiece; // downcasting para ChessPiece, já que a mesma era do tipo Piece, apenas uma posição ocupada no tabuleiro
-		
+		Piece capturedPiece = makeMove(source, target);
+		nextTurn();
+		return (ChessPiece) capturedPiece; // downcasting para ChessPiece, já que a mesma era do tipo Piece, apenas uma
+											// posição ocupada no tabuleiro
+
 	}
-	
+
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source); // a peça a ser removida é p. Com isso, criamos a variavel do tipo Piece e ela recebe o resultado do método removePiece tendo a posição de origem source como argumento
-		Piece capturedPiece = board.removePiece(target); // a peça capturada será a da posição de destino. Deixando assim, a posição vaga
+		Piece p = board.removePiece(source); // a peça a ser removida é p. Com isso, criamos a variavel do tipo Piece e
+												// ela recebe o resultado do método removePiece tendo a posição de
+												// origem source como argumento
+		Piece capturedPiece = board.removePiece(target); // a peça capturada será a da posição de destino. Deixando
+															// assim, a posição vaga
 		board.PlacePiece(p, target);
 		return capturedPiece;
 	}
-	
-	private void validateSourcePosition (Position position) {
+
+	private void validateSourcePosition(Position position) {
 		if (!board.theresAPiece(position)) {
 			throw new ChessException("Não existe peça na posição");
-		} 
+		}
+		if (currentPlayer != ((ChessPiece)board.piece(position)).getColor()) { // se a cor da peça nessa posição do tabuleiro for diferente do currentPlayer
+			throw new ChessException("Peça adiversária");
+		}
 		if (!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("Não há movimentos para a peça");
 		}
 	}
-	
-	private void validateTargetPosition (Position source, Position target) {
+
+	private void validateTargetPosition(Position source, Position target) {
 		if (!board.piece(source).possibleMove(target)) {
 			throw new ChessException("ERRO!");
 		}
+	}
+	
+	private void nextTurn() {
+		turn++;
+		currentPlayer= (currentPlayer== Color.WHITE) ? Color.BLACK : Color.WHITE; // se o currentPlayer for branco, então o proximo sera black. Caso contrario, sera white 
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) { // esse metodo já joga as coordenadas no
 																			// sistema do xadrez
 		board.PlacePiece(piece, new ChessPosition(column, row).toPosition()); // aqui chamamos o método
-																							// PlacePiece e damos como
-																							// argumento a piece,
-																							// declarando uma
-																							// chessPosition e aplicando
-																							// o método toPosition para
-																							// jogar às coordenadas da
-																							// matriz
+																				// PlacePiece e damos como
+																				// argumento a piece,
+																				// declarando uma
+																				// chessPosition e aplicando
+																				// o método toPosition para
+																				// jogar às coordenadas da
+																				// matriz
 
 	}
 
